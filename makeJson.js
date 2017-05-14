001b20597eed638fca721e64d120c7ac493e9a36
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-05-14 22:31:07 +0700

    fix

--- makeJson.js
+++ makeJson.js
@@ -7,62 +7,99 @@ var codes = new Russian();
 var fs = require('fs');
 var fileloaderHelper = require('numcap/tools/helpers/fileloader.js');
 
-
 var JsonMaker = function (config) {
 
-    this.makeExtendedCapacityJsonFile = function () {
+    var loadData = function () {
         var filenames = fileloaderHelper.getFilelistByExtension(config.dataDirectory, 'json');
-        var fullCapacity = [];
-        var resultFile = [];
-        
+        var capacity = [];
         for (var i = 0; i < filenames.length; i++) {
             var fileName = config.dataDirectory + filenames[i];
             var fileContentAsJson = JSON.parse(fs.readFileSync(fileName, 'utf8'));
             for (var j = 0; j < (fileContentAsJson.length - 1); j++) {
-                fullCapacity.push(fileContentAsJson[j]);
+                capacity.push(fileContentAsJson[j]);
             }
         }
+        return capacity;
+    };
 
-        codes.loadData(function () {
-            for (var i = 1; i < fullCapacity.length; i++) {
-                elementNumcap = fullCapacity[i];
-                var buffer = new phoneregion(elementNumcap.region);
-                buffer.fillShortName();
-                buffer.detectType();
+    var normalise = function (data) {
+        return data.toLowerCase().replace('—','-').replace(' - ','-').replace('- ','-').replace(' -','-');
+    };
 
-                if (buffer.shortName && typeof buffer.shortName == 'string') {
-                    if (buffer.shortName.indexOf('российская федерация') > -1 || buffer.shortName.indexOf('Байконур') > -1) {
-                        elementNumcap.region = {
-                            "title": buffer.baseName,
-                            "code": null,
-                            "county": null,
-                        };
-                    } else {
-                        codes.getRegionsByType(buffer.type, function (err, array) {
-                            for (var i = 0; i < array.length; i++) {
-                                if (buffer.shortName.indexOf(buffer.normalise(array[i].title)) > -1) {
-                                    elementNumcap.region = {
-                                        "title": array[i].title,
-                                        "code": array[i].code, 
-                                        "county": array[i].county,
-                                    };
-                                    break;
-                                }
-                            }
-                        });
-                    }
-                    resultFile.push(elementNumcap);
-                }
-            }
-            saveJson(resultFile, config.datafile);
+    var saveJson = function (obj, file) {
+        var string = JSON.stringify(obj, null, 2);
+        fs.writeFile(file, string, function (err) {
+            if (!err) console.log("JSON saved to " + file);
         });
     };
 
-    function saveJson (jsonContent, file) {
-        var json = JSON.stringify(jsonContent, null, 2);
-        fs.writeFile(file, json, function (err) {
-            if (!err) console.log("JSON saved to " + file);
+    var modificate = function (elementNumcap, buffer) {
+        
+        if (buffer.shortName.indexOf('российская федерация') > -1 || buffer.shortName.indexOf('байконур') > -1) {
+            elementNumcap.region = {
+                "title": buffer.baseName,
+                "code": null,
+                "county": null,
+            };
+        } else {
+            codes.getRegionsByType(buffer.type, function (err, array) {
+                
+                for (var i = 0; i < array.length; i++) {
+                    if (buffer.shortName.indexOf(normalise(array[i].title)) > -1) {
+                        elementNumcap.region = {
+                            "title": array[i].title,
+                            "code": array[i].code, 
+                            "county": array[i].county,
+                        };
+                        break;
+                    }
+                }
+            });
+        }
+        return elementNumcap;
+    }
+
+    var makeExtendedCapacityJsonFile = function () {
+        var fullCapacity = loadData();
+        console.log('loaded data')
+        var resultArr = [];
+
+        codes.loadData(() => {
+            resultArr = fullCapacity.map((elementNumcap, i) => { 
+                var buffer;
+                var f;
+                if (elementNumcap.region) {
+                    if (elementNumcap.region.indexOf('Республика Удмуртская') > -1) {
+                        console.log(elementNumcap);
+                        f = true;
+                    }
+                    buffer = new phoneregion(elementNumcap.region);
+                    elementNumcap = modificate(elementNumcap, buffer);
+                    if (f) {
+                        console.log(buffer);
+                        console.log(elementNumcap);
+                    }
+                } else {
+                    console.log(' ________________________ ! ________________________ ! ________________________')
+                    console.log(elementNumcap);
+                    elementNumcap = null;
+                }
+
+                return elementNumcap;
+            });
+
+            resultArr = resultArr.filter((element) => {
+                return element != null;
+            });
+
+            saveJson(resultArr, config.datafile);
         });
+    };
+
+
+
+    return {
+        makeExtendedCapacityJsonFile: makeExtendedCapacityJsonFile
     };
 };
 
